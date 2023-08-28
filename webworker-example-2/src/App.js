4676ed8489282d2afaf4d9eed24fbe706cf22cde
Sourabh Ingale <sourabh.ingale@zee.com>
2023-08-28 22:55:28 +0530

    web worker examples

---
+++ webworker-example-2/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import MyClass from "./myClass";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <MyClass />
+    </div>
+  );
+}
+
+export default App;
